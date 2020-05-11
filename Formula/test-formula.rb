ef099660d1c67b67e8337f620cac6af581204f41
Matthew John Cheetham <mjcheetham@github.com>
2020-05-11 14:18:39 +0100

    test-formula (formula): update to 2.3.4.1589203118
    Update the test-formula formula to version 2.3.4.1589203118.
    This modifies the Ruby file Formula/test-formula.rb.
    
    x = 2
    x.y = 2.3
    x.y.z = 2.3.4
    
    That's all folks! :rocket:

--- Formula/test-formula.rb
+++ Formula/test-formula.rb
@@ -1,8 +1,8 @@
 class TestFormula < Formula
 	desc "Test Formula"
 	homepage "https://example.com"
-	version "0.0"
-	sha256 "e99fa5e39fa055c318300f65353c8256fca7cc25c16212c73da2081c5a3637f7"
+	version "2.3.4.1589203118"
+	sha256 "blahblahblah1589203118"
 	url "https://example.com/v#{version.major_minor}/test-formula.#{version}.tar.gz"
 
 	def install
