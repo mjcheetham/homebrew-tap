9a490f84178193a060c4e6f67c0c78b097ebc77b
Matthew John Cheetham <mjcheetham@github.com>
2020-07-21 15:17:06 +0100

    test-cask: update to 2.0.901.40577

--- Casks/test-cask.rb
+++ Casks/test-cask.rb
@@ -1,6 +1,6 @@
 cask 'test-cask' do
-	version "20.2015.1595340829"
-	sha256 "blahblahblah1595340829"
+	version "2.0.901.40577"
+	sha256 "e6178a885c514cffd2ed59f565e058eeafbaa0aa7d3a9c20e32de49a0d247d37"
 
 	url "https://example.com/v20.2015.1595340829/test-formula.20.2015.1595340829.tar.gz"
 	name 'Test Cask'
