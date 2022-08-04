321f73d2c2ef4c66d4b525ab07ee04e068ccde79
Matthew John Cheetham <mjcheetham@github.com>
2022-08-04 20:46:36 +0100

    test-cask (cask): update to 20.2015.1659642391
    Update the test-cask cask to version 20.2015.1659642391.
    This modifies the Ruby file Casks/test-cask.rb.
    
    x = 20
    x.y = 20.2015
    x.y.z = 20.2015.1659642391
    
    That's all folks! :rocket:

--- Casks/test-cask.rb
+++ Casks/test-cask.rb
@@ -1,6 +1,6 @@
 cask 'test-cask' do
-	version "20.2015.1659635920"
-	sha256 "blahblahblah1659635920"
+	version "20.2015.1659642391"
+	sha256 "blahblahblah1659642391"
 
 	url "https://example.com/v20.2015.1659635920/test-formula.20.2015.1659635920.tar.gz"
 	name 'Test Cask'
