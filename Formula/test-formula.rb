22c7b7a8785d1680edaf9d966ae00779f12664c2
Victoria Dye <vdye@github.com>
2023-03-06 16:32:26 -0800

    test-formula (formula): update to 2.3.4.1678149145
    Update the test-formula formula to version 2.3.4.1678149145.
    This modifies the Ruby file Formula/test-formula.rb.
    
    x = 2
    x.y = 2.3
    x.y.z = 2.3.4
    
    That's all folks! :rocket:

--- Formula/test-formula.rb
+++ Formula/test-formula.rb
@@ -1,8 +1,8 @@
 class TestFormula < Formula
 	desc "Test Formula"
 	homepage "https://example.com"
-	version "2.3.4.1678147274"
-	sha256 "blahblahblah1678147274"
+	version "2.3.4.1678149145"
+	sha256 "blahblahblah1678149145"
 	url "https://example.com/v#{version.major_minor}/test-formula.#{version}.tar.gz"
 
 	def install
